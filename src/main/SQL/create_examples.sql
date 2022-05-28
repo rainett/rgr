37e5ea31de2d7bf699979f06d91b4f707523e8bc
rainett <86346775+valera3450zhma@users.noreply.github.com>
2022-05-28 20:08:58 +0300

    updated sql-start script

--- src/main/SQL/create_examples.sql
+++ src/main/SQL/create_examples.sql
@@ -6,83 +6,102 @@ DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS dishes;
 DROP TABLE IF EXISTS ordered_dishes;
 
-CREATE TABLE clients(
-    client_id INTEGER NOT NULL,
-    login varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
-    mail varchar(50) NOT NULL,
-    role varchar(50) NOT NULL,
-    PRIMARY KEY (client_id),
-    UNIQUE KEY (login)
+create table clients
+(
+    client_id int auto_increment
+        primary key,
+    login     varchar(50) not null,
+    password  varchar(50) not null,
+    mail      varchar(50) not null,
+    role      varchar(50) not null,
+    constraint login
+        unique (login)
 );
 
 INSERT INTO clients VALUES (1, 'admin', 'admin', 'admin@mail.com', 'manager');
 INSERT INTO clients VALUES (2, 'ivanov', '1234', 'ivanov@mail.com', 'client');
 INSERT INTO clients VALUES (3, 'petrov', '4321', 'petrov@mail.com', 'client');
 
-CREATE TABLE cards
+create table cards
 (
-    card_id INTEGER NOT NULL,
-    client_id INTEGER NOT NULL,
-    card_number CHAR(16),
-    card_till CHAR(5),
-    card_cvv CHAR(3),
-    PRIMARY KEY (card_id),
-    UNIQUE KEY (card_id)
+    card_id     int      not null,
+    client_id   int      not null,
+    card_number char(16) null,
+    card_till   char(5)  null,
+    card_cvv    char(3)  null,
+    constraint card_id
+        unique (card_id)
 );
+
+alter table cards
+    add primary key (card_id);
 
 INSERT INTO cards VALUES (1, 1, '1234123412341234', '05-24', '321');
 INSERT INTO cards VALUES (2, 1, '4321432143214321', '15-27', '123');
 INSERT INTO cards VALUES (3, 2, '4321111122223333', '06-23', '999');
 
-CREATE TABLE addresses(
-    address_id INTEGER NOT NULL,
-    client_id INTEGER NOT NULL,
-    city varchar(20) NOT NULL,
-    street varchar(30) NOT NULL,
-    house_number VARCHAR(10) NOT NULL,
-    apartment_number INTEGER,
-    PRIMARY KEY (address_id),
-    UNIQUE KEY (address_id)
+create table addresses
+(
+    address_id       int         not null,
+    client_id        int         not null,
+    city             varchar(20) not null,
+    street           varchar(30) not null,
+    house_number     varchar(10) not null,
+    apartment_number int         null,
+    constraint address_id
+        unique (address_id)
 );
+
+alter table addresses
+    add primary key (address_id);
 
 INSERT INTO addresses VALUES (1, 2, 'Lviv', 'Kulisha', '4', 305);
 INSERT INTO addresses VALUES (2, 1,'Lviv','Saharova','39',32);
 INSERT INTO addresses VALUES (3, 3, 'Lviv', 'Kulisha', '5', 216);
 INSERT INTO addresses (address_id, client_id, city, street, house_number) VALUES (4, 3, 'Lviv', 'Kulisha', '6');
 
-CREATE TABLE dishes(
-    dish_id INTEGER NOT NULL,
-    dish_name varchar(40) NOT NULL,
-    dish_price INTEGER NOT NULL,
-    PRIMARY KEY (dish_id),
-    UNIQUE KEY (dish_id)
+create table dishes
+(
+    dish_id    int auto_increment,
+    dish_name  varchar(40) not null,
+    dish_price int         not null,
+    dish_pic   varchar(30) null,
+    constraint dish_id
+        unique (dish_id)
 );
 
-INSERT INTO dishes VALUES (1, 'Sushi', 400);
-INSERT INTO dishes VALUES (2,'Varenyky', 100);
-INSERT INTO dishes VALUES (3, 'Borsch', 80);
+alter table dishes
+    add primary key (dish_id);
 
-CREATE TABLE ordered_dishes(
-    ordered_id INTEGER NOT NULL,
-    dish_id varchar(40) NOT NULL,
-    dish_amount INTEGER NOT NULL,
-    PRIMARY KEY (dish_id)
+INSERT INTO dishes VALUES (1, 'Sushi', 400, 'images/sushi.jpeg');
+INSERT INTO dishes VALUES (2,'Varenyky', 100, 'images/varenyky.jpeg');
+INSERT INTO dishes VALUES (3, 'Borsch', 80, 'images/borsch.jpeg');
+
+create table ordered_dishes
+(
+    ordered_id  int         not null,
+    dish_id     varchar(40) not null
+        primary key,
+    dish_amount int         not null
 );
 
 INSERT INTO ordered_dishes VALUES (1, 2, 2);
 INSERT INTO ordered_dishes VALUES (1, 1, 1);
 INSERT INTO ordered_dishes VALUES (2, 3, 1);
 
-CREATE TABLE orders(
-    order_id INTEGER NOT NULL,
-    client_id INTEGER NOT NULL,
-    ordered_id INTEGER NOT NULL,
-    card_id INTEGER NOT NULL,
-    address_id INTEGER NOT NULL,
-    PRIMARY KEY (order_id),
-    UNIQUE KEY (order_id)
+create table orders
+(
+    order_id   int not null,
+    client_id  int not null,
+    ordered_id int not null,
+    card_id    int not null,
+    address_id int not null,
+    constraint order_id
+        unique (order_id)
 );
+
+alter table orders
+    add primary key (order_id);
 
 INSERT INTO orders VALUES (2, 1, 1, 1, 2);
 
